9ef50aa2632edc3f957e1478e483911de88cd0a6
Ian Kerich <iankerich254@gmail.com>
2024-01-20 22:11:13 +0300

    Return if array is less than 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,7 +9,7 @@ void selection_sort(int *array, size_t size)
 	size_t i, j;
     int swapCount = 0, temp, current;
 
-	if (array == NULL)
+	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size; i++)
 	{
