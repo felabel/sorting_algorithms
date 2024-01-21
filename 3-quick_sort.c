b6a232c65f2dc2c3683a6a3103ef1fefa85c13a6
felabel <felicityabel2016@gmail.com>
2024-01-21 12:34:34 +0100

    debugging

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,15 +1,17 @@
 #include "sort.h"
 /**
  * swap_elements_fk - swaps two elements
+ * @array: array
  * @x: integer to be swapped
  * @d: integer to be swapped
  */
 
-void swap_elements_fk(int *x, int *d)
+void swap_elements_fk(int *array, ssize_t x, ssize_t d)
 {
-	int t = *x;
-	*x = *d;
-	*d = t;
+    int tmp;
+    tmp = array[x];
+    array[x] = array[d];
+    array[d] = tmp;
 }
 
 /**
@@ -23,20 +25,29 @@ void swap_elements_fk(int *x, int *d)
 
 int partition(int *array, int min_index_fk, int max_index_fk, size_t size)
 {
-	int pivot = array[max_index_fk], i = (min_index_fk - 1), j;
+    int pivot = array[max_index_fk];
+    int i = (min_index_fk - 1);
+    int j;
+    ssize_t current = min_index_fk;
 
-	for (j = min_index_fk; j <= max_index_fk - 1; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-			swap_elements_fk(&array[i], &array[j]);
-			print_array(array, size);
-		}
-	}
-	swap_elements_fk(&array[i + 1], &array[max_index_fk]);
-	print_array(array, size);
-	return (i + 1);
+    for (j = min_index_fk; j <= max_index_fk - 1; j++)
+    {
+        if (array[j] < pivot)
+        {
+            if (array[current] != array[j])
+            {
+                sswap_elements_fkwap(array, current, j);
+                print_array(array, size);
+            }
+            current++;
+        }
+    }
+    if (array[current] != array[max_index_fk])
+    {
+        swap_elements_fk(array, current, max_index_fk);
+        print_array(array, size);
+    }
+    return (current);
 }
 
 /**
@@ -49,13 +60,13 @@ int partition(int *array, int min_index_fk, int max_index_fk, size_t size)
 
 void rcsv_sort(int *array, int min_index_fk, int max_index_fk, size_t size)
 {
-	if (min_index_fk < max_index_fk)
-	{
-		int pvt_idx = partition(array, min_index_fk, max_index_fk, size);
+    if (min_index_fk < max_index_fk)
+    {
+        int pvt_idx = partition(array, min_index_fk, max_index_fk, size);
 
-		rcsv_sort(array, min_index_fk, pvt_idx - 1, size);
-		rcsv_sort(array, pvt_idx + 1, max_index_fk, size);
-	}
+        rcsv_sort(array, min_index_fk, pvt_idx - 1, size);
+        rcsv_sort(array, pvt_idx + 1, max_index_fk, size);
+    }
 }
 
 /**
@@ -66,7 +77,7 @@ void rcsv_sort(int *array, int min_index_fk, int max_index_fk, size_t size)
 
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-	rcsv_sort(array, 0, size - 1, size);
+    if (array == NULL || size < 2)
+        return;
+    rcsv_sort(array, 0, size - 1, size);
 }
