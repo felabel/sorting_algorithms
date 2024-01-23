0218fce53a9e677b552df52813d93c2e43a441b5
felabel <felicityabel2016@gmail.com>
2024-01-23 23:50:45 +0100

    cocktail task corrections

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,67 +1,66 @@
 #include "sort.h"
-#include <stdio.h>
-/**
- *swap_node - swap a node for his previous one
- *@node: node
- *@list: node list
- *Return: return a pointer to a node which was enter it
- */
-listint_t *swap_node(listint_t *node, listint_t **list)
-{
-	listint_t *back = node->prev, *current = node;
-	/*NULL, 19, 48, 9, 71, 13, NULL*/
 
-	back->next = current->next;
-	if (current->next)
-		current->next->prev = back;
-	current->next = back;
-	current->prev = back->prev;
-	back->prev = current;
-	if (current->prev)
-		current->prev->next = current;
-	else
-		*list = current;
-	return (current);
-}
 /**
- *cocktail_sort_list - this is a cocktail sort implementation
- *working on a double linked lists
+ *cocktail_sort_list - sorts an array with the cokatail method
  *@list: list
  */
 void cocktail_sort_list(listint_t **list)
 {
 	listint_t *node;
-	int swap_done = 1;
+	int swapped = 1;
 
 	if (list == '\0' || (*list) == '\0' || (*list)->next == '\0')
 		return;
 	node = *list;
-	while (swap_done == 1)
+	while (swapped == 1)
 	{
-		swap_done = 0;
+		swapped = 0;
 		while (node->next)
 		{
 			if (node->n > node->next->n)
 			{
 				node = swap_node(node->next, list);
-				swap_done = 1;
+				swapped = 1;
 				print_list(*list);
 			}
 			node = node->next;
 		}
-		if (swap_done == 0)
+		if (swapped == 0)
 			break;
-		swap_done = 0;
+		swapped = 0;
 		while (node->prev)
 		{
 			if (node->n < node->prev->n)
 			{
 				node = swap_node(node, list);
-				swap_done = 1;
+				swapped = 1;
 				print_list(*list);
 			}
 			else
 				node = node->prev;
 		}
 	}
+}
+
+/**
+ * swap_node - swaps the nodes of a doubly linked list
+ * @node: node to be swapped
+ * @list: list to be sorted
+ * Return: node
+ */
+
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *prv_node = node->prev, *cur_node = node;
+	prv_node->next = cur_node->next;
+	if (cur_node->next)
+		cur_node->next->prev = prv_node;
+	cur_node->next = prv_node;
+	cur_node->prev = prv_node->prev;
+	prv_node->prev = cur_node;
+	if (cur_node->prev)
+		cur_node->prev->next = cur_node;
+	else
+		*list = cur_node;
+	return (cur_node);
 }
